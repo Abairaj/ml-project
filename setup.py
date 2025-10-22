8570ad72254d2797c74d810bf474f4c89e5984a5
Abairaj <abairajpoinachi@gmail.com>
2025-10-22 18:22:05 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
-from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements(file_path:str)->List[str]:
+from setuptools import find_packages, setup
+
+
+def get_requirements(file_path: str) -> List[str]:
     """
     This function will return the list of requirements
     """
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n", "") for req in requirements]
 
-        if '-e .' in requirements:
-            requirements.remove('-e .')
+        if "-e ." in requirements:
+            requirements.remove("-e .")
     return requirements
 
+
 setup(
-    name='mlproject',
-    version='0.0.1',
+    name="mlproject",
+    version="0.0.1",
     packages=find_packages(),
-    author='Abai',
-    author_email='abairajpoinachi@gmail.com',
-    install_requires=get_requirements('requirements.txt'),
+    author="Abai",
+    author_email="abairajpoinachi@gmail.com",
+    install_requires=get_requirements("requirements.txt"),
 )
